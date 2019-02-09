a343711dc6291c01d45a3095770fedbc241d2b32
Matthew Groff <mattlgroff@gmail.com>
2019-02-09 17:08:52 -0600

    Fix server.js

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const packageName = 'vacation-planner';
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/waittimesangular'));
+app.use(express.static(__dirname + `/dist/${packageName}`));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/waittimesangular/index.html'));
+  res.sendFile(path.join(__dirname + `/dist/${packageName}/index.html`));
 });
 
+const port = process.env.PORT || 8080;
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+  console.log(`Express is hosting ${packageName} on port: ${port}`);
+});
