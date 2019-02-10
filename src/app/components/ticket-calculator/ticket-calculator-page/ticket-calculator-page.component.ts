81d78857f45244da299d6846d859c3f87630b2c1
Matthew Groff <mattlgroff@gmail.com>
2019-02-10 13:54:38 -0600

    Initial ticket calculator

--- src/app/components/ticket-calculator/ticket-calculator-page/ticket-calculator-page.component.ts
+++ src/app/components/ticket-calculator/ticket-calculator-page/ticket-calculator-page.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ticket-calculator-page',
   templateUrl: './ticket-calculator-page.component.html',
-  styleUrls: ['./ticket-calculator-page.component.scss']
+  styleUrls: ['./ticket-calculator-page.component.scss'],
 })
 export class TicketCalculatorPageComponent implements OnInit {
+  public singleDayPrice: number;
+  public numOfDays = 1;
+  public annualPassPrice: number;
+  public totalPrice: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public singleDayPriceChange(value: string): void {
+    this.singleDayPrice = parseInt(value, 10);
+    this.totalPrice = this.singleDayPrice * this.numOfDays;
   }
 
+  public numOfDaysChange(value: string): void {
+    this.numOfDays = parseInt(value, 10);
+    this.totalPrice = this.singleDayPrice * this.numOfDays;
+  }
 }
--- src/app/components/ticket-calculator/ticket-calculator.module.ts
+++ src/app/components/ticket-calculator/ticket-calculator.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { TicketCalculatorRoutingModule } from './ticket-calculator-routing.module';
 import { TicketCalculatorPageComponent } from './ticket-calculator-page/ticket-calculator-page.component';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 
 @NgModule({
+  imports: [
+    CommonModule,
+    TicketCalculatorRoutingModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   declarations: [TicketCalculatorPageComponent],
-  imports: [CommonModule, TicketCalculatorRoutingModule],
 })
 export class TicketCalculatorModule {}
