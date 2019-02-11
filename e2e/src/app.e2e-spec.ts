c6f8e591d88ae25bc6af9bc70d7bdf97cb3f4577
Matthew Groff <mattlgroff@gmail.com>
2019-02-10 23:11:48 -0600

    Update e2e protractor test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,28 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Vacation Planner', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display the project name in an <h1>', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to vacation-planner!');
+    expect(page.getTitleText()).toEqual('Vacation Planner');
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    }));
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      }),
+    );
   });
 });
